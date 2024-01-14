1bd197cd288fa218735eb46719b4cbcc18c70517
grufkork <griffin.hiscoke@me.com>
2024-01-14 18:49:53 +0100

    indent, lsp_signature

--- init.lua
+++ init.lua
@@ -50,34 +50,38 @@ vim.opt.rtp:prepend(lazypath)
 
 --vim.opt.laststatus = 0
 require("lazy").setup({
-	'nvim-lualine/lualine.nvim',
+    'nvim-lualine/lualine.nvim',
     'arkav/lualine-lsp-progress',
 
     'williamboman/mason.nvim',
-	'williamboman/mason-lspconfig.nvim',
+    'williamboman/mason-lspconfig.nvim',
     'neovim/nvim-lspconfig',
-	{'simrat39/rust-tools.nvim', lazy = true},
+    {'simrat39/rust-tools.nvim', lazy = true},
 
-  'hrsh7th/nvim-cmp', --Engine
-  'hrsh7th/cmp-path', --Paths
-  'hrsh7th/cmp-nvim-lsp', --From LSP
-  'hrsh7th/cmp-nvim-lsp-signature-help', --Inlay hints
-  'hrsh7th/cmp-nvim-lua',
-  'hrsh7th/cmp-buffer', -- From surrounding text,
-  'hrsh7th/cmp-vsnip',
-  'hrsh7th/vim-vsnip',
+    'hrsh7th/nvim-cmp', --Engine
+    'hrsh7th/cmp-path', --Paths
+    'hrsh7th/cmp-nvim-lsp', --From LSP
+    'hrsh7th/cmp-nvim-lsp-signature-help', --Inlay hints
+    'hrsh7th/cmp-nvim-lua',
+    'hrsh7th/cmp-buffer', -- From surrounding text,
+    'hrsh7th/cmp-vsnip',
+    'hrsh7th/vim-vsnip',
 
-  {
-    'folke/trouble.nvim',
-    dependencies = 'nvim-tree/nvim-web-devicons'
-  },
+    {
+        'folke/trouble.nvim',
+        dependencies = 'nvim-tree/nvim-web-devicons'
+    },
 
-  {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {}
-  },
-  
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {}
+    },
+
+    {
+        'ray-x/lsp_signature.nvim'
+    },
+
     'numToStr/Comment.nvim',
     --'tpope/vim-surround',
 
@@ -89,38 +93,38 @@ require("lazy").setup({
     },
 
     { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
-  --{ "ellisonleao/gruvbox.nvim", priority = 1000 },
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        contrast = "hard",
-         palette_overrides = {
-          dark0_hard = "#000000",
-        },
-      })
-    end,
-  },
+    --{ "ellisonleao/gruvbox.nvim", priority = 1000 },
+    {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        config = function()
+            require("gruvbox").setup({
+                contrast = "hard",
+                palette_overrides = {
+                    dark0_hard = "#000000",
+                },
+            })
+        end,
+    },
     "vimoxide/vim-cinnabar",
     "sainnhe/gruvbox-material"
-  -- 'simrat39/inlay-hints.nvim'
+    -- 'simrat39/inlay-hints.nvim'
 })
 
 
 
 -- Mason Setup
 require("mason").setup({
---    ui = {
---        icons = {
---            package_installed = "",
---            package_pending = "",
---            package_uninstalled = "",
---        },
---    }
+    --    ui = {
+        --        icons = {
+            --            package_installed = "",
+            --            package_pending = "",
+            --            package_uninstalled = "",
+            --        },
+            --    }
 })
 require("mason-lspconfig").setup{
-	ensure_installed = {"rust_analyzer", "vtsls", "html"}
+    ensure_installed = {"rust_analyzer", "vtsls", "html"}
 }
 require("lspconfig").vtsls.setup{}
 require("lspconfig").html.setup{}
@@ -129,16 +133,16 @@ require("lspconfig").omnisharp.setup{}
 --Autocomplete
 vim.opt.completeopt = {'menuone', 'noinsert', 'noselect'}
 
-local cmp = require'cmp'
+local cmp = require('cmp')
 
 cmp.setup({
-  sources = {
-    {name = "path"},
-    {name = "nvim_lsp"},
-    {name = "nvim_lsp_signature_help"},
-    {name = "nvim_lua"},
-    --{name = "buffer"},
-    {name = "vsnip"}
+    sources = {
+        {name = "path"},
+        {name = "nvim_lsp"},
+        {name = "nvim_lsp_signature_help"},
+        {name = "nvim_lua"},
+        --{name = "buffer"},
+        {name = "vsnip"}
     },
     mapping = {
         --['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -151,54 +155,57 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm({
-           behavior = cmp.ConfirmBehavior.Insert,
-          select = true,
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
         })
     },
     formatting = {
         fields = {'menu', 'abbr', 'kind'},
         format = function(entry, item)
-        local menu_icon ={
-            nvim_lsp = 'λ',
-            vsnip = '󱘢',
-            buffer = '󰊄',
-            path = '󰡰',
-            nvim_lua = ""
-        }
-        item.menu = menu_icon[entry.source.name] or "X"
-        return item
-    end,
+            local menu_icon ={
+                nvim_lsp = 'λ',
+                vsnip = '󱘢',
+                buffer = '󰊄',
+                path = '󰡰',
+                nvim_lua = ""
+            }
+            item.menu = menu_icon[entry.source.name] or "X"
+            return item
+        end,
     },
-  snippet = {
-    expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- completion = {'+'},
-    documentation = cmp.config.window.bordered(),
-  },
+    snippet = {
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- completion = {'+'},
+        documentation = cmp.config.window.bordered(),
+    },
 })
 
-vim.opt.updatetime = 300
+vim.opt.updatetime = 100
 vim.cmd([[
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 
 ]])
 
 
+require "lsp_signature".setup(cfg)
+
+
 local rt = require("rust-tools")
 
 rt.setup({
-  server = {
-		on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>aa", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
+    server = {
+        on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>aa", rt.code_action_group.code_action_group, { buffer = bufnr })
+        end,
+    },
 })
 
 local builtin = require('telescope.builtin')
@@ -216,27 +223,27 @@ vim.keymap.set('n', '<Leader>t', trouble.toggle, {})
 --Cosmetic 
 
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { "lua", "rust", "toml" },
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting=false,
-  },
-  ident = { enable = true }, 
+    ensure_installed = { "lua", "rust", "toml" },
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting=false,
+    },
+    ident = { enable = true }, 
 }
 
 require("lualine").setup {
-	options = {
-		theme = "gruvbox_dark"
-	},
-  sections = { 
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename', {"lsp_progress"}},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  }
+    options = {
+        theme = "gruvbox_dark"
+    },
+    sections = { 
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename', {"lsp_progress"}},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    }
 }
 -- require("inlay-hints").setup()
 
@@ -255,11 +262,11 @@ require("ibl").setup {
 }
 
 local sign = function(opts)
-  vim.fn.sign_define(opts.name, {
-    texthl = opts.name,
-    text = opts.text,
-    numhl = ''
-  })
+    vim.fn.sign_define(opts.name, {
+        texthl = opts.name,
+        text = opts.text,
+        numhl = ''
+    })
 end
 
 sign({name = 'DiagnosticSignError', text = ''})
