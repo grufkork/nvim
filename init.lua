1682ce111c1046369d81280a29a702c8a35dc6ce
grufkork <griffin.hiscoke@me.com>
2024-01-16 21:17:55 +0100

    Latex Macros

--- init.lua
+++ init.lua
@@ -72,7 +72,10 @@ require("lazy").setup({
 
     {
         "L3MON4D3/LuaSnip",
-        dependencies = { "rafamadriz/friendly-snippets", "saadparwaiz1/cmp_luasnip"},
+        dependencies = { 
+            -- "rafamadriz/friendly-snippets", 
+            "saadparwaiz1/cmp_luasnip"
+        },
     },
 
 
@@ -130,187 +133,190 @@ require("mason").setup({
             --            package_uninstalled = "",
             --        },
             --    }
-})
-require("mason-lspconfig").setup{
-    ensure_installed = {"rust_analyzer", "vtsls", "html"}
-}
-require("lspconfig").vtsls.setup{}
-require("lspconfig").html.setup{}
-require("lspconfig").omnisharp.setup{}
-
---Autocomplete
-vim.opt.completeopt = {'menuone', 'noinsert', 'noselect'}
-
-local cmp = require('cmp')
-
-
-cmp.setup({
-    sources = {
-        
-        { name = 'luasnip', option = { use_show_condition = false } },
-        --{name = "luasnip"},
-        {name = "path"},
-        {name = "nvim_lsp"},
-        {name = "nvim_lsp_signature_help"},
-        {name = "nvim_lua"},
-        --{name = "buffer"},
-        --{name = "vsnip"}
-    },
-    mapping = {
-        --['<C-p>'] = cmp.mapping.select_prev_item(),
-        --['<C-n>'] = cmp.mapping.select_next_item(),
-        -- Add tab support
-        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-        ['<Tab>'] = cmp.mapping.select_next_item(),
-        ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
         })
-    },
-    formatting = {
-        fields = {'menu', 'abbr', 'kind'},
-        format = function(entry, item)
-            local menu_icon ={
-                nvim_lsp = 'λ',
-                vsnip = '󱘢',
-                buffer = '󰊄',
-                path = '󰡰',
-                nvim_lua = ""
-            }
-            item.menu = menu_icon[entry.source.name] or "X"
-            return item
-        end,
-    },
-    snippet = {
-        expand = function(args)
-            require'luasnip'.lsp_expand(args.body)
-            --vim.fn["vsnip#anonymous"](args.body)
-        end,
-    },
-    window = {
-        -- completion = cmp.config.window.bordered(),
-        -- completion = {'+'},
-        documentation = cmp.config.window.bordered(),
-    },
-})
---cmp.setup.filetype("tex", {
---    sources = {
---        {name = 'vimtex'},
-        --{name = 'buffer'},
- --       {name = 'luasnip'}
---    }
---})
+        require("mason-lspconfig").setup{
+            ensure_installed = {"rust_analyzer", "vtsls", "html"}
+        }
+        require("lspconfig").vtsls.setup{}
+        require("lspconfig").html.setup{}
+        require("lspconfig").omnisharp.setup{}
 
-vim.opt.updatetime = 100
-vim.cmd([[
-autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+        --Autocomplete
+        vim.opt.completeopt = {'menuone', 'noinsert', 'noselect'}
 
-]])
+        local cmp = require('cmp')
 
 
-require "lsp_signature".setup(cfg)
+        cmp.setup({
+            sources = {
+
+                { name = 'luasnip', option = { use_show_condition = false } },
+                {name = 'vimtex'},
+                --{name = "luasnip"},
+                {name = "path"},
+                {name = "nvim_lsp"},
+                {name = "nvim_lsp_signature_help"},
+                {name = "nvim_lua"},
+                --{name = "buffer"},
+                --{name = "vsnip"}
+            },
+            mapping = {
+                --['<C-p>'] = cmp.mapping.select_prev_item(),
+                --['<C-n>'] = cmp.mapping.select_next_item(),
+                -- Add tab support
+                ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                ['<Tab>'] = cmp.mapping.select_next_item(),
+                ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.close(),
+                ['<CR>'] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Insert,
+                    select = true,
+                })
+            },
+            formatting = {
+                fields = {'menu', 'abbr', 'kind'},
+                format = function(entry, item)
+                    local menu_icon ={
+                        nvim_lsp = 'λ',
+                        vsnip = '󱘢',
+                        buffer = '󰊄',
+                        path = '󰡰',
+                        nvim_lua = ""
+                    }
+                    item.menu = menu_icon[entry.source.name] or "X"
+                    return item
+                end,
+            },
+            snippet = {
+                expand = function(args)
+                    require'luasnip'.lsp_expand(args.body)
+                    --vim.fn["vsnip#anonymous"](args.body)
+                end,
+            },
+            window = {
+                -- completion = cmp.config.window.bordered(),
+                -- completion = {'+'},
+                documentation = cmp.config.window.bordered(),
+            },
+        })
+        --cmp.setup.filetype("tex", {
+            --    sources = {
+                --        {name = 'vimtex'},
+                --{name = 'buffer'},
+                --       {name = 'luasnip'}
+                --    }
+                --})
+
+                vim.opt.updatetime = 100
+                vim.cmd([[
+                autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+
+                ]])
 
 
-
-local rt = require("rust-tools")
-
-rt.setup({
-    server = {
-        on_attach = function(_, bufnr)
-            -- Hover actions
-            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            vim.keymap.set("n", "<Leader>aa", rt.code_action_group.code_action_group, { buffer = bufnr })
-        end,
-    },
-})
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-vim.keymap.set('n', '<Leader>ar', vim.lsp.buf.rename, {})
-
-local trouble = require("trouble")
-vim.keymap.set('n', '<Leader>t', trouble.toggle, {})
-
---Cosmetic 
-
-require('nvim-treesitter.configs').setup {
-    ensure_installed = { "lua", "rust", "toml" },
-    auto_install = true,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting=false,
-        disable = {"latex"}
-    },
-    ident = { enable = true }, 
-}
-
-require("lualine").setup {
-    options = {
-        theme = "gruvbox_dark"
-    },
-    sections = { 
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename', {"lsp_progress"}},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-    }
-}
--- require("inlay-hints").setup()
-
-vim.o.background = "dark" -- or "light" for light mode
-vim.cmd([[colorscheme gruvbox]])
-
-vim.opt.termguicolors = true
-vim.cmd [[highlight IndentBlanklineIndent1 guifg=#202020 gui=nocombine]]
-vim.cmd [[highlight IndentBlanklineIndent2 guibg=#101010 gui=nocombine]]
-
-local highlight = {
-    "CursorColumn",
-    "Whitespace",
-}
-require("ibl").setup {
-}
-
-local sign = function(opts)
-    vim.fn.sign_define(opts.name, {
-        texthl = opts.name,
-        text = opts.text,
-        numhl = ''
-    })
-end
-
-sign({name = 'DiagnosticSignError', text = ''})
-sign({name = 'DiagnosticSignWarn', text = ''})
-sign({name = 'DiagnosticSignHint', text = ''})
-sign({name = 'DiagnosticSignInfo', text = ''})
-
-require("Comment").setup()
+                require "lsp_signature".setup(cfg)
 
 
 
+                local rt = require("rust-tools")
 
-vim.opt.conceallevel=2
-require("luasnip.loaders.from_vscode").lazy_load()
+                rt.setup({
+                    server = {
+                        on_attach = function(_, bufnr)
+                            -- Hover actions
+                            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+                            -- Code action groups
+                            vim.keymap.set("n", "<Leader>aa", rt.code_action_group.code_action_group, { buffer = bufnr })
+                        end,
+                    },
+                })
 
-local ls = require'luasnip'
--- vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+                local builtin = require('telescope.builtin')
+                vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+                vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+                vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+                vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
--- vim.keymap.set({"i", "s"}, "<C-E>", function()
--- 	if ls.choice_active() then
--- 		ls.change_choice(1)
--- 	end
--- end, {silent = true})
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+                vim.keymap.set('n', '<Leader>ar', vim.lsp.buf.rename, {})
+
+                local trouble = require("trouble")
+                vim.keymap.set('n', '<Leader>t', trouble.toggle, {})
+
+                --Cosmetic 
+
+                require('nvim-treesitter.configs').setup {
+                    ensure_installed = { "lua", "rust", "toml" },
+                    auto_install = true,
+                    highlight = {
+                        enable = true,
+                        additional_vim_regex_highlighting=false,
+                        disable = {"latex"}
+                    },
+                    ident = { enable = true }, 
+                }
+
+                require("lualine").setup {
+                    options = {
+                        theme = "gruvbox_dark"
+                    },
+                    sections = { 
+                        lualine_a = {'mode'},
+                        lualine_b = {'branch', 'diff', 'diagnostics'},
+                        lualine_c = {'filename', {"lsp_progress"}},
+                        lualine_x = {'encoding', 'fileformat', 'filetype'},
+                        lualine_y = {'progress'},
+                        lualine_z = {'location'}
+                    }
+                }
+                -- require("inlay-hints").setup()
+
+                vim.o.background = "dark" -- or "light" for light mode
+                vim.cmd([[colorscheme gruvbox]])
+
+                vim.opt.termguicolors = true
+                vim.cmd [[highlight IndentBlanklineIndent1 guifg=#202020 gui=nocombine]]
+                vim.cmd [[highlight IndentBlanklineIndent2 guibg=#101010 gui=nocombine]]
+
+                local highlight = {
+                    "CursorColumn",
+                    "Whitespace",
+                }
+                require("ibl").setup {
+                }
+
+                local sign = function(opts)
+                    vim.fn.sign_define(opts.name, {
+                        texthl = opts.name,
+                        text = opts.text,
+                        numhl = ''
+                    })
+                end
+
+                sign({name = 'DiagnosticSignError', text = ''})
+                sign({name = 'DiagnosticSignWarn', text = ''})
+                sign({name = 'DiagnosticSignHint', text = ''})
+                sign({name = 'DiagnosticSignInfo', text = ''})
+
+                require("Comment").setup()
+
+
+
+
+                vim.opt.conceallevel=2
+                --require("luasnip.loaders.from_vscode").lazy_load()
+
+                local ls = require'luasnip'
+                -- vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+                vim.keymap.set({"i", "s"}, "<Tab>", function() ls.jump( 1) end, {silent = true})
+                vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+
+                -- vim.keymap.set({"i", "s"}, "<C-E>", function()
+                    -- 	if ls.choice_active() then
+                    -- 		ls.change_choice(1)
+                    -- 	end
+                    -- end, {silent = true})
+
+                    require("mysnips")
---
+++ lua/mysnips.lua
@@ -0,0 +1,106 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+local extras = require("luasnip.extras")
+local l = extras.lambda
+local rep = extras.rep
+local p = extras.partial
+local m = extras.match
+local n = extras.nonempty
+local dl = extras.dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local conds = require("luasnip.extras.expand_conditions")
+local postfix = require("luasnip.extras.postfix").postfix
+local types = require("luasnip.util.types")
+local parse = require("luasnip.util.parser").parse_snippet
+local ms = ls.multi_snippet
+local k = require("luasnip.nodes.key_indexer").new_key
+
+ls.add_snippets("all", {
+    s("bgn", 
+    {
+        t'\\begin{', 
+        i(1), 
+        t({'}', ''}),
+        t'\t',
+        i(0),
+        t({'', '\\end{'}),
+        extras.rep(1), t'}'
+    }),
+
+    postfix(
+    "hat",
+    {
+        f(function(_, parent) 
+            return "\\hat{" .. parent.snippet.env.POSTFIX_MATCH .. "}"
+        end, 
+        {}),
+
+    }),
+    s("mm", 
+    {
+        t'$', 
+        i(1), 
+        t'$'
+    }),
+    s("eq", 
+    {
+        t({'\\begin{equation}', '\t'}),
+        i(0),
+        t({'', '\\end{equation}'}),
+    }),
+    s("->",
+    {
+        t'\\to'
+    }),
+
+    s("/",{
+        t'\\frac{',
+        i(1),
+        t'}{',
+        i(2),
+        t'}',
+        i(0)
+    }),
+
+    s("fun",{
+        t 'f: \\mathbb{R}^',
+        i(1),
+        t '\\to \\mathbb{R}'
+
+    }),
+
+    s("ln",
+    {t('\\\\')}),
+
+    s("dim", {
+        t '\\mathbb{R}^', i(1)
+    }),
+
+    s("par", 
+    {
+        t'\\paragraph{',
+        i(1),
+        t '}', i(0)
+    }),
+
+    s("lim", 
+    {
+        t'\\lim _{',
+        i(1),
+        t '\\to',
+        i(2),
+        t '}', i(0)
+    }),
+
+})
